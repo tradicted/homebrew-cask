7984ce93a8c3fabc6c654309b262e668dbda0cdd
Robert <robertgorak91@googlemail.com>
2026-06-06 20:54:55 +0700

    Add cask to root for local testing

---
+++ Casks/tradicted-trading-journal.rb
@@ -0,0 +1,17 @@
+cask "tradicted-trading-journal" do
+  version "1.0.0"
+  sha256 "badd455121d30a21f6aba1bfc987060e541c8a2721e51a6315bcc20afb94645b"
+
+  url "https://github.com/tradicted/tradicted-journal/releases/download/v#{version}/Tradicted.Journal-#{version}.dmg"
+  name "Tradicted Trading Journal"
+  desc "Free open-source desktop trading journal"
+  homepage "https://www.tradicted.com"
+
+  app "Tradicted Journal.app"
+
+  zap trash: [
+    "~/Library/Application Support/tradicted-trading-journal",
+    "~/Library/Preferences/com.tradicted.journal.plist",
+    "~/Library/Saved Application State/com.tradicted.journal.savedState",
+  ]
+end
